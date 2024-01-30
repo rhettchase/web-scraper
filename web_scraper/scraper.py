46289dfa029cffc97d710d9e3e915bdfe8c7668e
RHETT CHASE <rhettjchase@gmail.com>
2024-01-30 15:17:19 -0800

    adds get_citations_needed_count and get_citations_needed_report

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -3,8 +3,35 @@ from bs4 import BeautifulSoup
 
 url = "https://en.wikipedia.org/wiki/History_of_San_Diego"
 
-response = requests.get(url)
+def get_citations_needed_report(url):
+    response = requests.get(url)
+    markup = response.text
+    
+    soup = BeautifulSoup(markup, 'html.parser')
+    citation_needed = soup.select('a[title="Wikipedia:Citation needed"]')
+    
+    citation_count = get_citations_needed_count(url)
+    
+    citation_output = f"Citation needed for the following {citation_count} passage(s):\n\n"
 
-soup = BeautifulSoup(response.text, 'html.parser')
+    for link in citation_needed:
+        grandparent = link.find_parent('p')  # Find the parent <p> tag
+        if grandparent:
+            citation_output += grandparent.get_text() + "\n"
+    return citation_output
 
-print(soup.prettify())
+def get_citations_needed_count(url):
+    response = requests.get(url)
+    markup = response.text
+    
+    soup = BeautifulSoup(markup, 'html.parser')
+    citation_needed = soup.select('a[title="Wikipedia:Citation needed"]')
+    
+    count = 0
+    
+    for link in citation_needed:
+        count += 1
+    return count
+
+print(get_citations_needed_report(url))
+print(get_citations_needed_count(url))
